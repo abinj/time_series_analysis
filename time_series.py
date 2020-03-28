c8bd4f461b5e77225706eefab9b17d255bc08fcf
abinj <abinmj656@gmail.com>
2020-03-28 18:36:25 -0400

    Double exonential smoothing done

--- time_series.py
+++ time_series.py
@@ -79,3 +79,32 @@ def plot_exponential_smoothing(series, alphas):
     plt.show()
 
 plot_exponential_smoothing(data.Passengers, [0.05, 0.3, 0.1])
+
+# Double exponential smoothing
+def double_exponential_smoothing(series, alpha, beta):
+    result = [series[0]]
+    for n in range(1, len(series) + 1):
+        if n == 1:
+            level, trend = series[0], series[1] - series[0]
+        if n >= len(series): #forcasting
+            value = result[-1]
+        else:
+            value = series[n]
+        last_level, level = level, alpha * value + (1 - alpha) * (level + trend)
+        trend = beta * (level - last_level) + (1-beta) * trend
+        result.append(level + trend)
+    return result
+
+def plot_double_exponential_smoothing(series, alphas, betas):
+    plt.figure(figsize=(17,8))
+    for alpha in alphas:
+        for beta in betas:
+            plt.plot(double_exponential_smoothing(series, alpha, beta), label="Alpha {}, beta {}".format(alpha, beta))
+    plt.plot(series.values, label= "Actual")
+    plt.legend(loc="best")
+    plt.axis('tight')
+    plt.title("Double Exponential Smoothing")
+    plt.grid(True)
+    plt.show()
+
+plot_double_exponential_smoothing(data.Passengers, alphas=[0.9, 0.02], betas=[0.9, 0.02])
