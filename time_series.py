357fb3c47e0fe7f0af4019343c1bbf729806deb0
abinj <abinmj656@gmail.com>
2020-03-28 18:49:55 -0400

    Modelling done

--- time_series.py
+++ time_series.py
@@ -8,6 +8,10 @@ sns.set()
 from sklearn.metrics import r2_score, median_absolute_error, mean_absolute_error
 from sklearn.metrics import median_absolute_error, mean_squared_error, mean_squared_log_error
 
+from scipy.optimize import minimize
+import statsmodels.tsa.api as smt
+import statsmodels.api as sm
+
 
 def mean_average_percentage_error(y_true, y_pred):
     return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
@@ -108,3 +112,30 @@ def plot_double_exponential_smoothing(series, alphas, betas):
     plt.show()
 
 plot_double_exponential_smoothing(data.Passengers, alphas=[0.9, 0.02], betas=[0.9, 0.02])
+
+# Modelling
+def tsplot(y, lags=None, figsize=(12, 7), syle='bmh'):
+    if not isinstance(y, pd.Series):
+        y = pd.Series(y)
+    with plt.style.context(style='bmh'):
+        fig = plt.figure(figsize=figsize)
+        layout = (2, 2)
+        ts_ax = plt.subplot2grid(layout, (0, 0), colspan=2)
+        acf_ax = plt.subplot2grid(layout, (1, 0))
+        pacf_ax = plt.subplot2grid(layout, (1, 1))
+
+        y.plot(ax=ts_ax)
+        p_value = sm.tsa.stattools.adfuller(y)[1]
+        ts_ax.set_title('Time Series Analysis Plots\n Dickey-Fuller: p={0:.5f}'.format(p_value))
+        smt.graphics.plot_acf(y, lags=lags, ax=acf_ax)
+        smt.graphics.plot_pacf(y, lags=lags, ax=pacf_ax)
+        plt.tight_layout()
+        plt.show()
+
+
+tsplot(data.Passengers, lags=30)
+
+# Take the first difference to remove to make the process stationary
+data_diff = data.Passengers - data.Passengers.shift(1)
+
+tsplot(data_diff[1:], lags=30)
