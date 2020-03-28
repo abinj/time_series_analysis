864142a2e4bce9fd64390dca6d5217734a09b4fb
abinj <abinmj656@gmail.com>
2020-03-28 19:28:07 -0400

    SARIMA done

--- time_series.py
+++ time_series.py
@@ -12,6 +12,10 @@ from scipy.optimize import minimize
 import statsmodels.tsa.api as smt
 import statsmodels.api as sm
 
+from tqdm import tqdm
+
+from itertools import product
+
 
 def mean_average_percentage_error(y_true, y_pred):
     return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
@@ -139,3 +143,67 @@ tsplot(data.Passengers, lags=30)
 data_diff = data.Passengers - data.Passengers.shift(1)
 
 tsplot(data_diff[1:], lags=30)
+
+# Sarima
+# Set initial values and some bounds
+ps = range(0, 5)
+d = 1
+qs = range(0, 5)
+Ps = range(0, 5)
+D = 1
+Qs = range(0, 5)
+s = 5
+
+# Create a list with all possible combinations of parameters
+parameters = product(ps, qs, Ps, Qs)
+parameters_list = list(parameters)
+len(parameters_list)
+
+
+# Train many SARIMA models to find the best set of parameters
+def optimize_SARIMA(parameters_list, d, D, s):
+    """
+        Return dataframe with parameters and corresponding AIC
+
+        parameters_list - list with (p, q, P, Q) tuples
+        d - integration order
+        D - seasonal integration order
+        s - length of season
+    """
+
+    results = []
+    best_aic = float('inf')
+
+    for param in tqdm(parameters_list):
+        try:
+            model = sm.tsa.statespace.SARIMAX(data.Passengers, order=(param[0], d, param[1]),
+                                              seasonal_order=(param[2], D, param[3], s)).fit(disp=-1)
+        except:
+            continue
+
+        aic = model.aic
+
+        # Save best model, AIC and parameters
+        if aic < best_aic:
+            best_model = model
+            best_aic = aic
+            best_param = param
+        results.append([param, model.aic])
+
+    result_table = pd.DataFrame(results)
+    result_table.columns = ['parameters', 'aic']
+    # Sort in ascending order, lower AIC is better
+    result_table = result_table.sort_values(by='aic', ascending=True).reset_index(drop=True)
+
+    return result_table
+
+
+result_table = optimize_SARIMA(parameters_list, d, D, s)
+
+# Set parameters that give the lowest AIC (Akaike Information Criteria)
+p, q, P, Q = result_table.parameters[0]
+
+best_model = sm.tsa.statespace.SARIMAX(data.Passengers, order=(p, d, q),
+                                       seasonal_order=(P, D, Q, s)).fit(disp=-1)
+
+print(best_model.summary())
