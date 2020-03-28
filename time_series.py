d96fa53ffe350198430ab7a87900963d647e1c11
abinj <abinmj656@gmail.com>
2020-03-28 17:40:03 -0400

    Moving average found

--- time_series.py
+++ time_series.py
@@ -27,3 +27,36 @@ plt.ylabel('Number of passengers')
 plt.xlabel('day')
 plt.grid(False)
 plt.show()
+
+
+def plot_moving_average(series, window, plot_intervals=False, scale=1.96):
+    rolling_mean = series.rolling(window=window).mean()
+
+    plt.figure(figsize=(17,8))
+    plt.title('Moving average\n window size = {}'.format(window))
+    plt.plot(rolling_mean, 'g', label='Rolling mean trend')
+
+    # Plot confidence intervals for smoothed values
+    if plot_intervals:
+        mae = mean_absolute_error(series[window:], rolling_mean[window:])
+        deviation = np.std(series[window:] - rolling_mean[window:])
+        lower_bound = rolling_mean - (mae + scale * deviation)
+        upper_bound = rolling_mean + (mae + scale * deviation)
+        plt.plot(upper_bound, 'r--', label='Upper bound / Lower bound')
+        plt.plot(lower_bound, 'r--')
+
+    plt.plot(series[window:], label='Actual values')
+    plt.legend(loc='best')
+    plt.grid(True)
+    plt.show()
+
+#Smooth by the previous 5 days (by week)
+plot_moving_average(data.Passengers, 5)
+
+#Smooth by the previous month (30 days)
+plot_moving_average(data.Passengers, 30)
+
+#Smooth by previous quarter (90 days)
+plot_moving_average(data.Passengers, 90, plot_intervals=True)
+
+
